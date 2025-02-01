92196bed68273f6c5a72f1878bf6714b9c00cb30
Fernikaan <falvic00@estudiantes.unileon.es>
2025-02-01 20:45:34 +0100

    Problema de la BBDD resuelto

--- app/src/main/java/com/fnd/miflix/database/AppDatabase.kt
+++ app/src/main/java/com/fnd/miflix/database/AppDatabase.kt
@@ -2,7 +2,6 @@ package com.fnd.miflix.database
 
 import android.content.Context
 import androidx.room.*
-
 import com.fnd.miflix.models.User
 import com.fnd.miflix.models.Notificacion
 import com.fnd.miflix.models.ContenidoSeguido
@@ -10,11 +9,9 @@ import com.fnd.miflix.models.DAO.NotificacionesDAO
 import com.fnd.miflix.models.DAO.ContenidoSeguidoDAO
 import com.fnd.miflix.models.DAO.UserDao
 
-// Define la base de datos y las entidades que usa
 @Database(entities = [User::class, Notificacion::class, ContenidoSeguido::class], version = 3, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
-    // Métodos abstractos para acceder a los DAOs
     abstract fun usuariosDao(): UserDao
     abstract fun notificacionDao(): NotificacionesDAO
     abstract fun contenidoSeguidoDao(): ContenidoSeguidoDAO
@@ -23,14 +20,15 @@ abstract class AppDatabase : RoomDatabase() {
         @Volatile
         private var INSTANCE: AppDatabase? = null
 
-        // Patrón Singleton para obtener la instancia de la base de datos
         fun getDatabase(context: Context): AppDatabase {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     AppDatabase::class.java,
-                    "app_database"  // Nombre de la base de datos
-                ).build()
+                    "app_database"
+                )
+                    .fallbackToDestructiveMigration() // ⚠ Esto borra los datos al actualizar
+                    .build()
                 INSTANCE = instance
                 instance
             }
